3b8c81bab0f62613606a623ad4e5ff35d931538a
dksan23 <47032393+dksan23@users.noreply.github.com>
2019-02-02 21:47:32 +0530

    Add files via upload

---
+++ PREDICTAGSONSTACKOVERFLOW.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  2 19:20:08 2019
+
+@author: HeavyD
+"""
+
+import sys
+sys.path.append("..")
+from common.download_utils import download_week1_resources
+
+download_week1_resources()
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from ast import literal_eval
+import pandas as pd
+import numpy as np
+def readata(filename):
+    data=pd.read_csv(filename,sep='\t')
+    data['tags']=data['tags'].apply(literal_eval)
+    return data
+train=readata('data/train.tsv')
+validation=readata('data/validation.tsv')
+test=pd.read_csv('data/test.tsv',sep='\t')
+xtrain,ytrain=train['title'].values,train['tags'].values
+xvalid,yvalid=validation['title'].values,validation['tags'].values
+xtest=test['title'].values
+import re
+REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
+BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
+STOPWORDS = set(stopwords.words('english'))
+
+def text_prepare(text):
+    """
+        text: a string
+        
+        return: modified initial string
+    """
+    text =text.lower() # lowercase text
+    text =re.sub(REPLACE_BY_SPACE_RE," ",text) # replace REPLACE_BY_SPACE_RE symbols by space in text
+    text =re.sub(BAD_SYMBOLS_RE,"",text) # delete symbols which are in BAD_SYMBOLS_RE from text
+    text1=text.split()# delete stopwords from text
+    text=" ".join(word for word in text1 if word not in (STOPWORDS))
+    return text
+xtrain=[text_prepare(text) for text in xtrain]
+xvalid=[text_prepare(text) for text in xvalid]
+xtest=[text_prepare(text) for text in xtest]
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(min_df=5, max_df=0.9, ngram_range=(1, 2),token_pattern='(\S+)')
+xtrain=vectorizer.fit_transform(xtrain)
+xvalid=vectorizer.transform(xvalid)
+xtest=vectorizer.transform(xtest)
+from collections import defaultdict
+tagcounts=defaultdict(int)
+
+for tags in ytrain:
+    for tag in tags:
+        tagcounts[tag]+=1
+from sklearn.preprocessing import MultiLabelBinarizer
+mlv=MultiLabelBinarizer(classes=(sorted(tagcounts.keys())))
+ytrain=mlv.fit_transform(ytrain)
+yvalid=mlv.fit_transform(yvalid)   
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import LinearSVC
+def classifer(xtrain,ytrain):
+    svm=LinearSVC()
+    orc=OneVsRestClassifier(svm,n_jobs=-1)
+    return orc.fit(xtrain,ytrain)
+classified=classifer(xtrain,ytrain)
+prediction=classified.predict(xvalid) 
+predictionscore=classified.decision_function(xvalid)   
+prediction=mlv.inverse_transform(prediction)
+yval=mlv.inverse_transform(yval)
+yval[3]
+prediction[3]
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score 
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import recall_score
+print(accuracy_score(yval, prediction))
+print(f1_score(yval, prediction, average='weighted'))
+print(average_precision_score(yval, prediction))  
+
